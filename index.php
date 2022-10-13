e00565883d6a00006ef8fb678074a59c9ebf3544
naimul3070 <16103070@iubat.edu>
2022-10-13 17:40:22 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>php</title>
+    <h1>First PHP code</h1>
+</head>
+<body>
+<?php
+echo "<p>Hello World!</p>";
+echo "<p> This is Naimul Islam </p>" ;
+echo "Today is " . date("Y-m-d") . "<br>";
+?> 
+
+</body>
+</html>
